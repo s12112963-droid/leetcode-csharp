37c03ee8f79ea938588de221085c15d66bec0305
Rasmiya <rasmiya.thaher44@gmail.com>
2026-02-14 22:30:36 +0200

    Add Reverse String problem solution

---
+++ Reverse String.cs
@@ -0,0 +1,29 @@
+﻿/*
+LeetCode Problem: 344. Reverse String
+Link: https://leetcode.com/problems/reverse-string/description/
+
+Description:
+Write a function that reverses a character array in-place.
+The function should modify the input array directly
+and use only constant extra memory.
+*/
+
+using System;
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        char x;
+        int left = 0;
+        int right = (s.Length) - 1;
+        while (left < right)
+        {
+            x = s[left];
+            s[left] = s[right];
+            s[right] = x;
+            left++;
+            right--;
+        }
+
+    }
+}
