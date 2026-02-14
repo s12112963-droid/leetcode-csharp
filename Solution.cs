498a062ad4a5a8f6e76893dffc8c02c6985e44b4
Rasmiya <rasmiya.thaher44@gmail.com>
2026-02-14 16:10:23 +0200

    Add Two Sum solution

---
+++ Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+
+            }
+
+        }
+        return new int[] { };
+
+    }
+}
